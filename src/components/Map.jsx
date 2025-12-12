de3baf21d6ed773f1cd3acd0b36541c23e731543
Michael Duffy <maduffy02@gmail.com>
2025-12-12 01:32:54 -0500

    Added an interactive map to the homepage using leaflet js and tiles from stadiamaps. Added the logo to the assets folder. Made minor changes to css files (not permanent changes).

---
+++ src/components/Map.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+function Map() {
+
+    useEffect(() => {
+
+        // to prevent duplicate initialization (necessary so strictmode doesn't break this code in prod)
+        if (L.DomUtil.get("map")?._leaflet_id) {
+        return;
+        }
+
+        const map = L.map("map").setView([38.9072, -77.0369], 13);
+
+        // tile layer (OpenStreetMap)
+        L.tileLayer("https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png", {
+            maxZoom: 19,
+            detectRetina: true
+        }).addTo(map);
+
+        // sample marker
+        L.marker([38.9072, -77.036]).addTo(map).bindPopup("Washington, DC");
+    }, []);
+
+    return (
+        <div
+            id="map"
+            style={{
+                height: "80vh",
+                width: "100%",
+            }}
+        />
+    );
+}
+
+export default Map;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Map from "../components/Map";
 
 function HomePage() {
   return (
     <>
         <Navbar />
-        <div className="home-container">
-        <h1>CourtFinder</h1>
-        <input
-            type="text"
-            placeholder="Search for courts..."
-            className="search-box"
-        />
-        <button className="near-me-btn">Find Courts Near Me</button>
-        </div>
+        <Map />
     </>
   );
 }
