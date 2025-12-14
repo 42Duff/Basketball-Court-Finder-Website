f5fd787312012cdcb5e4063fdd9b8082e76466f4
Michael Duffy <maduffy02@gmail.com>
2025-12-13 23:26:10 -0500

    Created files for filters and legend components. Created a constant for court status that specifies a color and label for each court status. Slight adjustments to navbar.

---
+++ src/constants/courtStatus.js
@@ -0,0 +1,18 @@
+export const COURT_STATUS = {
+    BUSY: {
+        label: "Busy",
+        color: "c92a2a"
+    },
+    MEDIUM: {
+        label: "Moderately Busy",
+        color: "ff8310"
+    },
+    LOW: {
+        label: "Not That Busy",
+        color: "2ca21e"
+    },
+    UNREPORTED: {
+        label: "No Recent Data",
+        color: "c5c5c5"
+    }
+}
